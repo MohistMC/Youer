10d1a9731f24b5f4fb45ba6f6853d664ee55ee6b
Mgazul <mgazul@163.com>
2025-09-08 01:55:48 +0800

    Add /info cmd <command name>

--- src/main/java/com/mohistmc/youer/commands/InfoCommand.java
+++ src/main/java/com/mohistmc/youer/commands/InfoCommand.java
@@ -5,11 +5,13 @@ import com.mohistmc.youer.util.I18n;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -17,18 +19,18 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Mgazul by MohistMC
- * @date 2023/8/1 20:00:00
+ * @date 2025/8/31 20:00:00
  */
 public class InfoCommand extends Command {
 
     public InfoCommand(String name) {
         super(name);
         this.description = "Youer info commands";
-        this.usageMessage = "/info [item|block|entity]";
+        this.usageMessage = "/info [item|block|entity|cmd]";
         this.setPermission("youer.command.info");
     }
 
-    private final List<String> params = List.of("item", "block", "entity");
+    private final List<String> params = List.of("item", "block", "entity", "cmd");
 
     @Override
     public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, String[] args) {
@@ -37,6 +39,12 @@ public class InfoCommand extends Command {
             for (String param : params) {
                 if (param.toLowerCase().startsWith(args[0].toLowerCase())) {
                     list.add(param);
+                }
+            }
+        } else if (args.length == 2 && "cmd".equalsIgnoreCase(args[0]) && (sender.isOp() || testPermission(sender))) {
+            for (String command : Bukkit.getServer().getCommandMap().getKnownCommands().keySet()) {
+                if (command.toLowerCase().startsWith(args[1].toLowerCase())) {
+                    list.add(command);
                 }
             }
         }
@@ -183,11 +191,77 @@ public class InfoCommand extends Command {
                 }
                 return true;
             }
+            case "cmd" -> {
+                if (args.length < 2) {
+                    sender.sendMessage(ChatColor.RED + I18n.as("info.cmd.usage"));
+                    return false;
+                }
+
+                String commandName = args[1];
+                PluginCommand pluginCommand = player.getServer().getPluginCommand(commandName);
+
+                if (pluginCommand == null) {
+                    Command unknownCommand = player.getServer().getCommandMap().getCommand(commandName);
+                    if (unknownCommand == null) {
+                        sender.sendMessage(ChatColor.YELLOW + I18n.as("info.cmd.non_plugin_command"));
+                        return false;
+                    } else {
+                        sendCommandInfo(sender, unknownCommand);
+                        return true;
+                    }
+                }
+
+                sendCommandInfo(sender, pluginCommand);
+                return true;
+            }
 
             default -> {
                 sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
                 return false;
             }
         }
+    }
+
+    public void sendCommandInfo(CommandSender sender, Command pluginCommand) {
+        String commandLabel = pluginCommand.getLabel();
+        String permission = pluginCommand.getPermission();
+        String commandDescription = pluginCommand.getDescription();
+        String aliases = String.join(", ", pluginCommand.getAliases());
+
+        String firstLine = "&6&m----------------&r&6 " + I18n.as("info.cmd.non_plugin_title") + " &6&m----------------";
+        String message = firstLine + "\n" +
+                "&e" + I18n.as("info.cmd.source") + " &f" + commandLabel + "\n" +
+                "&e" + I18n.as("info.cmd.command_description") + " &f" + commandDescription + "\n" +
+                "&e" + I18n.as("info.cmd.permission") + " &f" + (permission != null ? permission : I18n.as("info.cmd.none")) + "\n" +
+                "&e" + I18n.as("info.cmd.aliases") + " &f" + (aliases.isEmpty() ? I18n.as("info.cmd.none") : aliases);
+
+        sender.sendMessage(message.replace("&", "§"));
+    }
+
+    public void sendCommandInfo(CommandSender sender, PluginCommand pluginCommand) {
+        String pluginName = pluginCommand.getPlugin().getName();
+        String pluginVersion = pluginCommand.getPlugin().getDescription().getVersion();
+        String pluginDescription = pluginCommand.getPlugin().getDescription().getDescription();
+        List<String> pluginAuthors = pluginCommand.getPlugin().getDescription().getAuthors();
+        String permission = pluginCommand.getPermission();
+        String commandDescription = pluginCommand.getDescription();
+        String aliases = String.join(", ", pluginCommand.getAliases());
+        String authorsDisplay = (pluginAuthors.size() > 3) ? (String.join(", ", pluginAuthors.subList(0, 3)) + " ...") : String.join(", ", pluginAuthors);
+
+        if (pluginDescription != null && pluginDescription.length() > 40) {
+            pluginDescription = pluginDescription.substring(0, 40) + " ...";
+        }
+
+        String firstLine = "&6&m----------------&r&6 " + I18n.as("info.cmd.plugin_title") + " &6&m----------------";
+        String message = firstLine + "\n" +
+                "&e" + I18n.as("info.cmd.plugin_name") + " &f" + pluginName + "\n" +
+                "&e" + I18n.as("info.cmd.plugin_version") + " &f" + pluginVersion + "\n" +
+                "&e" + I18n.as("info.cmd.plugin_description") + " &f" + pluginDescription + "\n" +
+                "&e" + I18n.as("info.cmd.plugin_authors") + " &f" + authorsDisplay + "\n" +
+                "&e" + I18n.as("info.cmd.command_description") + " &f" + commandDescription + "\n" +
+                "&e" + I18n.as("info.cmd.permission") + " &f" + (permission != null ? permission : I18n.as("info.cmd.none")) + "\n" +
+                "&e" + I18n.as("info.cmd.aliases") + " &f" + (aliases.isEmpty() ? I18n.as("info.cmd.none") : aliases);
+
+        sender.sendMessage(message.replace("&", "§"));
     }
 }
