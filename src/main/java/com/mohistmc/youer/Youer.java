751d4990d4d61bac99ee29a9fd5d72e014014b63
Mgazul <mgazul@163.com>
2026-02-06 03:48:23 +0800

    Update neoforge snapshot-6

---
+++ src/main/java/com/mohistmc/youer/Youer.java
@@ -0,0 +1,12 @@
+package com.mohistmc.youer;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+
+public class Youer {
+    public static final String NAME = "Youer";
+
+    public Youer(IEventBus modEventBus, Dist dist, ModContainer container) {
+    }
+}
