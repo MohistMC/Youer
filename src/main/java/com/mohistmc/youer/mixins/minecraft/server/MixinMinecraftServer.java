a86213266c6eef652097ad2a2f6cb59deeca2348
Mgazul <mgazul@163.com>
2026-02-27 16:36:10 +0800

    color motd

---
+++ src/main/java/com/mohistmc/youer/mixins/minecraft/server/MixinMinecraftServer.java
@@ -0,0 +1,13 @@
+package com.mohistmc.youer.mixins.minecraft.server;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * @author Mgazul
+ * @date 2026/2/27 15:55
+ */
+@Mixin(MinecraftServer.class)
+public class MixinMinecraftServer {
+
+}
