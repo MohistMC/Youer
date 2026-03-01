9d3b76e70ea20f36a210e773c831453556b02df6
Mgazul <mgazul@163.com>
2026-03-01 16:26:29 +0800

    Add custom_disabled_spawner configuration

--- src/main/java/com/mohistmc/youer/YouerConfig.java
+++ src/main/java/com/mohistmc/youer/YouerConfig.java
@@ -120,6 +120,7 @@ public class YouerConfig {
     public static int custom_lava_speed_normal;
     public static int custom_lava_speed_nether;
     public static boolean custom_fix_flat;
+    public static boolean custom_disabled_spawner;
     public static boolean player_modlist_blacklist_enable;
     public static boolean player_modlist_blacklist_use_real_feedback;
     public static String player_modlist_blacklist_failurereasons;
@@ -308,6 +309,7 @@ public class YouerConfig {
         custom_lava_speed_normal = getInt("custom.lava_speed.normal", 30);
         custom_lava_speed_nether = getInt("custom.lava_speed.nether", 10);
         custom_fix_flat = getBoolean("custom.fix_flat", false);
+        custom_disabled_spawner = getBoolean("custom.disabled_spawner", false);
 
         player_modlist_blacklist_enable = getBoolean("player_modlist_blacklist.enable", false);
         player_modlist_blacklist_use_real_feedback = getBoolean("player_modlist_blacklist.use_real_feedback", false);
